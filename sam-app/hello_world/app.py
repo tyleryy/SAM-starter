98a48b53075a399ad5f603735f77007267eb1653
Tyler Yu <tyleryy@uci.edu>
2024-01-13 02:46:11 -0800

    updated lambda

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 # import requests
 
@@ -36,7 +37,8 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello everyone",
+            "time": time.time(),
             # "location": ip.text.replace("\n", "")
         }),
     }
